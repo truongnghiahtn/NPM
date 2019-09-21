6c1672c2d9c7b150115583756c87013a319771d6
truongnghia <truongnghiahtn@gmail.com>
2019-09-21 12:51:35 +0700

    21/9/update

--- src/app/entities/home/trang-chi-tiet/trang-chi-tiet.module.ts
+++ src/app/entities/home/trang-chi-tiet/trang-chi-tiet.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TrangChiTietRoutingModule } from './trang-chi-tiet-routing.module';
 import { TrangChiTietComponent } from './trang-chi-tiet.component';
@@ -12,7 +13,7 @@ import { DanhGiaComponent } from './danh-gia/danh-gia.component';
   declarations: [TrangChiTietComponent, LichchieuComponent, ThongtinComponent, DanhGiaComponent],
   imports: [
     CommonModule,
-    TrangChiTietRoutingModule
+    TrangChiTietRoutingModule,FormsModule
   ]
 })
 export class TrangChiTietModule { }
