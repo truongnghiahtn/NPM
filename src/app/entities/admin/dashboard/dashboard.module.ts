b1d0b32e41ab19a91b5cc5c94316c89a907cdc8a
truongnghia <truongnghiahtn@gmail.com>
2019-09-25 17:11:54 +0700

    ok

--- src/app/entities/admin/dashboard/dashboard.module.ts
+++ src/app/entities/admin/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
@@ -8,14 +9,16 @@ import { QuanLyPhimComponent } from './quan-ly-phim/quan-ly-phim.component';
 import { QuanLyTinTucComponent } from './quan-ly-tin-tuc/quan-ly-tin-tuc.component';
 import { AdminHeaderComponent } from './admin-header/admin-header.component';
 import { MaterialModule } from 'src/app/common/material/material.module';
+import { ItemTinTucComponent } from './quan-ly-tin-tuc/item-tin-tuc/item-tin-tuc.component';
+import { ModalNewsComponent } from './quan-ly-tin-tuc/modal-news/modal-news.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, QuanLyNguoiDungComponent, QuanLyPhimComponent, QuanLyTinTucComponent, AdminHeaderComponent],
+  declarations: [DashboardComponent, QuanLyNguoiDungComponent, QuanLyPhimComponent, QuanLyTinTucComponent, AdminHeaderComponent, ItemTinTucComponent, ModalNewsComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule,MaterialModule
+    DashboardRoutingModule,MaterialModule,FormsModule
   ]
 })
 export class DashboardModule { }
