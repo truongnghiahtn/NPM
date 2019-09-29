a860b1810272ddad245f51bceb2b83880bd0b1d8
minh <17520758@gm.uit.edu.vn>
2019-09-29 20:46:42 +0700

    upload minh 8:46/29/9

--- src/app/entities/admin/dashboard/dashboard.module.ts
+++ src/app/entities/admin/dashboard/dashboard.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard.component';
-import { QuanLyNguoiDungComponent } from './quan-ly-nguoi-dung/quan-ly-nguoi-dung.component';
-import { QuanLyPhimComponent } from './quan-ly-phim/quan-ly-phim.component';
-import { QuanLyTinTucComponent } from './quan-ly-tin-tuc/quan-ly-tin-tuc.component';
-import { AdminHeaderComponent } from './admin-header/admin-header.component';
-import { MaterialModule } from 'src/app/common/material/material.module';
-
-
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { DashboardComponent } from "./dashboard.component";
+import { QuanLyNguoiDungComponent } from "./quan-ly-nguoi-dung/quan-ly-nguoi-dung.component";
+import { QuanLyPhimComponent } from "./quan-ly-phim/quan-ly-phim.component";
+import { QuanLyTinTucComponent } from "./quan-ly-tin-tuc/quan-ly-tin-tuc.component";
+import { AdminHeaderComponent } from "./admin-header/admin-header.component";
+import { MaterialModule } from "src/app/common/material/material.module";
+import { AdminDangKyModule } from "./../admin-dang-ky/admin-dang-ky.module";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [DashboardComponent, QuanLyNguoiDungComponent, QuanLyPhimComponent, QuanLyTinTucComponent, AdminHeaderComponent],
+  declarations: [
+    DashboardComponent,
+    QuanLyNguoiDungComponent,
+    QuanLyPhimComponent,
+    QuanLyTinTucComponent,
+    AdminHeaderComponent
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule,MaterialModule
+    DashboardRoutingModule,
+    MaterialModule,
+    AdminDangKyModule,
+    FormsModule
   ]
 })
-export class DashboardModule { }
+export class DashboardModule {}
--- src/app/entities/admin/dashboard/quan-ly-nguoi-dung/quan-ly-nguoi-dung.component.ts
+++ src/app/entities/admin/dashboard/quan-ly-nguoi-dung/quan-ly-nguoi-dung.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { DataService } from "src/app/shared/services/data.service";
 
 @Component({
-  selector: 'app-quan-ly-nguoi-dung',
-  templateUrl: './quan-ly-nguoi-dung.component.html',
-  styleUrls: ['./quan-ly-nguoi-dung.component.scss']
+  selector: "app-quan-ly-nguoi-dung",
+  templateUrl: "./quan-ly-nguoi-dung.component.html",
+  styleUrls: ["./quan-ly-nguoi-dung.component.scss"]
 })
 export class QuanLyNguoiDungComponent implements OnInit {
-
-  constructor() { }
-
+  @Output() event = new EventEmitter();
+  @Output() them = new EventEmitter();
+  @Output() sua = new EventEmitter();
+  searchInput: string = "";
+  constructor(private dataSV: DataService) {}
+  mangNguoiDung: any = [];
+  trangThai: boolean = false;
+  mangTimKiem: any = [];
   ngOnInit() {
+    this.layDSNguoiDung();
   }
 
+  // Call api
+  layDSNguoiDung() {
+    const uri =
+      "http://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=GP01";
+    this.dataSV.get(uri).subscribe(
+      data => {
+        console.log(data);
+        this.mangNguoiDung = data;
+        this.mangTimKiem = this.mangNguoiDung;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  xoaNguoiDung(item) {
+    console.log(item);
+    const uri = `http://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/XoaNguoiDung?TaiKhoan=${item.taiKhoan}`;
+    this.dataSV.delete(uri).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  // Output event
+  themNguoiDung() {
+    this.trangThai = true;
+    this.them.emit(this.trangThai);
+  }
+  capNhatThongTin(item) {
+    this.event.emit(item);
+    this.trangThai = false;
+    this.sua.emit(this.trangThai);
+  }
+
+  timKiem(event) {
+    this.mangTimKiem = [];
+    this.searchInput = event.target.value;
+    this.searchInput = this.searchInput.toLowerCase();
+    console.log(this.searchInput);
+    if (this.mangNguoiDung !== []) {
+      this.mangNguoiDung.map(item => {
+        item.hoTen = item.hoTen.toLowerCase();
+        if (item.hoTen.indexOf(this.searchInput) !== -1) {
+          this.mangTimKiem.push(item);
+        }
+      });
+    }
+  }
 }
